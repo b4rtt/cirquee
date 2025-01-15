884088288fa72376a663470080846bbe3f174204
b4rtt <b4rt11@gmail.com>
2025-01-15 14:47:11 +0100

    Refactor Home component to use ZustandTest for state management
    - Removed direct Zustand store integration from the Home component.
    - Integrated ZustandTest component to handle state management and UI interactions.
    - Simplified the Home component by eliminating count display and state manipulation buttons.
    
    These changes enhance code modularity and improve the overall structure of the Home component.

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -2,14 +2,9 @@ import { router } from 'expo-router';
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
-import useStore, { StoreState } from '../../src/store/useStore';
+import ZustandTest from '@/components/ZustandTest';
 
 const Home = () => {
-  const count = useStore((state: StoreState) => state.count);
-  const increase = useStore((state: StoreState) => state.increase);
-  const decrease = useStore((state: StoreState) => state.decrease);
-  const reset = useStore((state: StoreState) => state.reset);
-
   return (
     <View
       style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue' }}>
@@ -18,10 +13,7 @@ const Home = () => {
 
       <View style={{ height: 100 }} />
 
-      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Count: {count}</Text>
-      <Button title="Increase" onPress={increase} />
-      <Button title="Decrease" onPress={decrease} />
-      <Button title="Reset" onPress={reset} />
+      <ZustandTest />
     </View>
   );
 };
---
+++ src/components/ZustandTest.tsx
@@ -0,0 +1,20 @@
+import { Button, Text } from 'react-native';
+
+import useStore, { StoreState } from '../../src/store/useStore';
+
+const ZustandTest = () => {
+  const count = useStore((state: StoreState) => state.count);
+  const increase = useStore((state: StoreState) => state.increase);
+  const decrease = useStore((state: StoreState) => state.decrease);
+  const reset = useStore((state: StoreState) => state.reset);
+  return (
+    <>
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Count: {count}</Text>
+      <Button title="Increase" onPress={increase} />
+      <Button title="Decrease" onPress={decrease} />
+      <Button title="Reset" onPress={reset} />
+    </>
+  );
+};
+
+export default ZustandTest;
