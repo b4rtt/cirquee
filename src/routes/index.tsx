9d82b70111861cc1b06f99b80fece57129b92165
b4rtt <b4rt11@gmail.com>
2025-01-27 11:35:24 +0100

    Configure SVG support and update Metro configuration
    - Added react-native-svg-transformer to package.json for improved SVG handling
    - Updated metro.config.js to support SVG imports and transformer configuration
    - Imported and used an SVG logo in the Home route component to demonstrate SVG support
    
    This change enhances the project's ability to work with SVG assets and improves the build configuration for handling vector graphics.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { router } from 'expo-router';
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
+import Logo from '../../assets/images/icons/test.svg';
+
 import { useTranslations } from '@/hooks/useTranslations';
 
 const Home = () => {
@@ -11,6 +13,8 @@ const Home = () => {
       style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}>
       <Text>{t('home.title')}</Text>
       <Button title="Go to Home" onPress={() => router.push('/home')} />
+
+      <Logo width={100} height={100} />
     </View>
   );
 };
